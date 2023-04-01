2189e2d6e95848a42b94854c9fb26f597cb4a9a6
SJH-Jason <129576330+SJH-Jason@users.noreply.github.com>
2023-04-02 01:23:40 +0800

    測試錯誤

---
+++ CsharpHomework/_07HWMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpHomework
+{
+    public partial class _07HWMethod : Form
+    {
+        public _07HWMethod()
+        {
+            InitializeComponent();
+        }
+    }
+}
